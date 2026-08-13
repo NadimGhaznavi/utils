a0ff277fc4fce06ba19fb18cbfb68c684596e73f
root <root@paris.osoyalce.com>
2026-08-13 05:09:42 -0400

    Feat: Add a reboot reminder

--- bin/patch-node.sh
+++ bin/patch-node.sh
@@ -3,11 +3,11 @@
 LOG=/var/log/patch-nodes.log
 
 NOW=$(date)
-echo "$NOW: Starting patch process" >> $LOG
-apt update >> $LOG
-apt -y autoremove  >> $LOG
-apt -y upgrade >> $LOG
-apt -y dist-upgrade >> $LOG
+echo "$NOW: Starting patch process" | tee -a $LOG
+apt update | tee -a $LOG
+apt -y autoremove | tee -a $LOG
+apt -y upgrade | tee -a $LOG
+apt -y dist-upgrade | tee -a $LOG
 NOW=$(date)
-echo "$NOW: Patch process complete" >> $LOG
-
+echo "$NOW: Patch process complete" | tee -a $LOG
+echo "Reboot your system a soon as possible"
